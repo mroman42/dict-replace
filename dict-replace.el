d9d88b339adbadb0cdbc056c81a662a3a102942c
M42 <mromang08@gmail.com>
2016-07-09 15:54:55 +0200

    Initial version

---
+++ dict-replace.el
@@ -0,0 +1,30 @@
+(defun dict-translate ()
+  (interactive)
+  (let ((tword (dict-lookup (thing-at-point 'word))))
+    (other-window 1)
+    (setq bounds (bounds-of-thing-at-point 'word))
+    (setq pos1 (car bounds))
+    (setq pos2 (cdr bounds))
+    (delete-region pos1 pos2)
+    (insert tword)
+    )
+  )
+
+
+(defun dict-lookup (word)
+  (switch-to-buffer-other-window "*map*")
+  (goto-char (point-min))
+
+  (if (word-search-forward word nil t)
+      (progn
+       (word-search-forward "-> ")
+       (thing-at-point 'word)
+       )
+    
+    (let ((trans-word (read-from-minibuffer "Enter translation for this word: ")))
+	  (goto-char (point-max))
+	  (insert (format "\n%s -> %s" word trans-word))
+	  (symbol-value 'trans-word)
+	  )
+    ) 
+  )
